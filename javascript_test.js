2e5081e7acac3609ec8b81f2beb4a04e18fcd368
Aermyx <54551414+Aermyx@users.noreply.github.com>
2020-09-04 16:26:17 -0400

    Résultat Final

--- javascript_test.js
+++ javascript_test.js
@@ -1,14 +1,21 @@
 
-const listChoix = [
+/*const listChoix = [
     ["✊", "✋", "✌️"],
     ["✊🏻", "✋🏻", "✌🏻"],
     ["✊🏽", "✋🏽", "✌🏽"],
     ["✊🏿", "✋🏿", "✌🏿"],
-];
+];*/
 
 //Game
 class Game {
     constructor() {
+        this.listChoix = [
+            ["✊", "✋", "✌️"],
+            ["✊🏻", "✋🏻", "✌🏻"],
+            ["✊🏽", "✋🏽", "✌🏽"],
+            ["✊🏿", "✋🏿", "✌🏿"],
+        ];
+
         this.afficherNomJoueur();
         this.afficherActionJoueur();
         this.afficherActionCpu();
@@ -26,10 +33,10 @@ class Game {
         const listCouleurPeau = document.form01.Peau;
         const couleurPeau = listCouleurPeau.value;
         //Action Joueur
-        const choixMain = document.form01.action.selectedIndex;
+        this.choixMain = document.form01.action.selectedIndex;
         //Résultats Joueur
-        const couleurJoueur = listChoix[couleurPeau];
-        const actionJoueur = couleurJoueur[choixMain];
+        const couleurJoueur = this.listChoix[couleurPeau];
+        const actionJoueur = couleurJoueur[this.choixMain];
         //Affichage Joueur
         const resultatJoueur = document.querySelector(".choixJoueur");
         resultatJoueur.innerHTML = `<span>${actionJoueur}</span>`;
@@ -38,16 +45,41 @@ class Game {
     afficherActionCpu() {
         //Couleur CPU
         const couleurRandom = Math.round( Math.random() * 3 );
-        const couleurCpu = listChoix[couleurRandom];
+        const couleurCpu = this.listChoix[couleurRandom];
 
         //Action CPU
-        const actionRandom = Math.round( Math.random() * 2 );
-        const actionCpu = couleurCpu[actionRandom];
+        this.actionRandom = Math.round( Math.random() * 2 );
+        const actionCpu = couleurCpu[this.actionRandom];
 
         //Affichage CPU
         const resultatCpu = document.querySelector(".choixCpu");
         resultatCpu.innerHTML = `<span>${actionCpu}</span>`;
-        return actionRandom;
+    }
+
+    resultatMatch() {
+
+        const halfJoueur = document.querySelector(".joueur");
+        const halfCpu = document.querySelector(".cpu");
+        const resultat = document.querySelector(".resultat");
+
+        if(this.choixMain == this.actionRandom) {
+            halfJoueur.style.setProperty("--fondJoueur", "#3F88C5");
+            halfCpu.style.setProperty("--fondCpu", "#3F88C5");
+            resultat.innerHTML = "<span>Égalité</span>";
+        }
+        else if(
+            this.choixMain == 0 && this.actionRandom == 1 ||
+            this.choixMain == 1 && this.actionRandom == 2 ||
+            this.choixMain == 2 && this.actionRandom == 0 ) {
+                halfJoueur.style.setProperty("--fondJoueur", "#D16D82");
+                halfCpu.style.setProperty("--fondCpu", "#7FD8BE");
+                resultat.innerHTML = "<span>Défaite</span>";
+            }
+        else {
+            halfJoueur.style.setProperty("--fondJoueur", "#7FD8BE");
+            halfCpu.style.setProperty("--fondCpu", " #D16D82");
+            resultat.innerHTML = "<span>Victoire</span>";
+        }
     }
 }
 
