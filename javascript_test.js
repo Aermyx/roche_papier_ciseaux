98585a52d19b99e5c1e8d437676759c3ccb8c73b
Aermyx <54551414+Aermyx@users.noreply.github.com>
2020-09-04 10:18:16 -0400

    Résultat Joueur

--- javascript_test.js
+++ javascript_test.js
@@ -3,7 +3,8 @@
 class Game {
     constructor() {
         this.afficherNomJoueur();
-        this.couleurJoueur();
+        this.afficherActionJoueur();
+        this.afficherActionCpu();
     }
     
     afficherNomJoueur() {
@@ -12,82 +13,47 @@ class Game {
         resultatNomJoueur.innerHTML = `<span>${valeurNomJoueur}</span>`;
     }
 
-    couleurJoueur() {
-        const listCouleurJoueur = document.form01.Peau;
-        let couleurJoueur = "";
-        couleurJoueur = listCouleurJoueur.value;
-        console.log(couleurJoueur);
-        return couleurJoueur;
-        
+    afficherActionJoueur() {
+        //Couleur
+        const listCouleurPeau = document.form01.Peau;
+        const couleurPeau = listCouleurPeau.value;
+        //Action
+        const choixMain = document.form01.action.selectedIndex;
+        //Résultats
+        const listChoix = [
+            ["✊", "✋", "✌️"],
+            ["✊🏻", "✋🏻", "✌🏻"],
+            ["✊🏽", "✋🏽", "✌🏽"],
+            ["✊🏿", "✋🏿", "✌🏿"],
+        ];
+        const couleurJoueur = listChoix[couleurPeau];
+        const actionJoueur = couleurJoueur[choixMain];
+        //Affichage
+        const resultatJoueur = document.querySelector(".choixJoueur");
+        resultatJoueur.innerHTML = `<span>${actionJoueur}</span>`;
+    }
+
+    afficherActionCpu() {
+        const couleurCpu = listChoix[indexRandom];
+        console.log(couleurCpu)
     }
 }
-const game = new Game();
 
 
 //INTERACTION----------------------------------------------------------
 
 
 //Jouer
-const btnJouer = document.querySelector("#jouer");
+const btnJouer = document.querySelector(".jouer");
 const btnRejouer = document.querySelector(".btnRejouer");
 
 btnJouer.addEventListener("click", () => {
-
-//Nom du Joueur
-    
-//Couleur Joueur
-
-
-//Choix Joueur
-let choixJoueur = document.form01.action.selectedIndex;
-let jeuJoueur = "";
-
-/*if(couleurJoueur == 0 && choixJoueur == 0) {
-    jeuJoueur = "✊";
-}
-else if(couleurJoueur == 0 && choixJoueur == 1) {
-    jeuJoueur = "✋";
-}
-else if(couleurJoueur == 0 && choixJoueur == 2) {
-    jeuJoueur = "✌️";
-}
-else if(couleurJoueur == 1 && choixJoueur == 0) {
-    jeuJoueur = "✊🏻";
-}
-else if(couleurJoueur == 1 && choixJoueur == 1) {
-    jeuJoueur = "✋🏻";
-}
-else if(couleurJoueur == 1 && choixJoueur == 2) {
-    jeuJoueur = "✌🏻";
-}
-else if(couleurJoueur == 2 && choixJoueur == 0) {
-    jeuJoueur = "✊🏽";
-}
-else if(couleurJoueur == 2 && choixJoueur == 1) {
-    jeuJoueur = "✋🏽";
-}
-else if(couleurJoueur == 2 && choixJoueur == 2) {
-    jeuJoueur = "✌🏽";
-}
-else if(couleurJoueur == 3 && choixJoueur == 0) {
-    jeuJoueur = "✊🏿";
-}
-else if(couleurJoueur == 3 && choixJoueur == 1) {
-    jeuJoueur = "✋🏿";
-}
-else {
-    jeuJoueur = "✌🏿";
-}*/
-
-//Résultat Joueur
-const actionJoueur = document.querySelector(".choixJoueur");
-actionJoueur.innerHTML = `<span>${jeuJoueur}</span>`;
 
 //Disparaître Formulaire
     document.querySelector(".boxFormulaire").classList.add("none");
     document.querySelector(".fondFormulaire").classList.add("none");
 
-    new Game();
+    const game = new Game();
 })
 
 
